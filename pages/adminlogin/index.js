9409697911eddc4f50b3f7e36210117b2e476ef5
HansongProgramming <hlf1850@students.uc-bcf.edu.ph>
2025-07-01 10:01:37 +0800

    changes to frontend: fixed bar charts and positioning, made them readable

--- pages/adminlogin/index.js
+++ pages/adminlogin/index.js
@@ -9,13 +9,9 @@ let vistorCategory = document
   .getElementById("visitorCategory")
   .getContext("2d");
 
-let returningVsNew = document
-  .getElementById("returningVsNew")
-  .getContext("2d");
+let returningVsNew = document.getElementById("returningVsNew").getContext("2d");
 
-let officeActivity = document
-  .getElementById("officeActivity")
-  .getContext("2d");
+let officeActivity = document.getElementById("officeActivity").getContext("2d");
 
 Chart.defaults.backgroundColor = "#64E4B1";
 
@@ -23,39 +19,89 @@ let visitorTimeLine = new Chart(myBarChart, {
   type: "bar",
   data: {
     labels: [
-      "8:00", "9:00", "10:00", "11:00", "12:00",
-      "1:00", "2:00", "3:00", "4:00", "5:00",
+      "8:00",
+      "9:00",
+      "10:00",
+      "11:00",
+      "12:00",
+      "1:00",
+      "2:00",
+      "3:00",
+      "4:00",
+      "5:00",
     ],
-    datasets: [{
-      label: "Number of Visitors",
-      data: [15, 5, 7, 20, 30, 20, 5, 2, 5, 10],
-      borderWidth: 1,
-    }],
-   },
-    options: {
-      scales: {
-        y: { beginAtZero: true },
-        x: { title: { display: true, text: "Time of Day" } }
+    datasets: [
+      {
+        label: "Number of Visitors",
+        data: [15, 5, 7, 20, 30, 20, 5, 2, 5, 10],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        beginAtZero: true,
+        ticks: {
+          color: "#ffffff",
+        },
+        title: {
+          display: true,
+          color: "#ffffff",
+        },
+      },
+      x: {
+        title: {
+          display: true,
+          text: "Time of Day",
+          color: "#ffffff",
+        },
+        ticks: {
+          color: "#ffffff",
+        },
       },
     },
+    plugins: {
+      legend: {
+        labels: {
+          color: "#ffffff",
+        },
+      },
+      title: {
+        color: "#ffffff",
+      },
+    },
+  },
 });
 
 let Visitor_category = new Chart(visitorCategory, {
   type: "doughnut",
   data: {
     labels: ["Guests", "Staff"],
-    datasets: [{
-      label: "Visitor Category",
-      data: [100, 50],
-      backgroundColor: ["#64E4B1", "#008650"],
-      borderWidth: 0,
-    }],
+    datasets: [
+      {
+        label: "Visitor Category",
+        data: [100, 50],
+        backgroundColor: ["#64E4B1", "#008650"],
+        borderWidth: 0,
+      },
+    ],
   },
   options: {
     cutout: 70,
     plugins: {
-      legend: { position: "bottom" },
-      title: { display: true, text: "Visitor Statistics" },
+      legend: {
+        position: "bottom",
+        labels: {
+          color: "#ffffff",
+        },
+      },
+      title: {
+        display: true,
+        text: "Visitor Statistics",
+        color: "#ffffff",
+      },
     },
   },
 });
@@ -64,18 +110,25 @@ let Returning_vs_New = new Chart(returningVsNew, {
   type: "doughnut",
   data: {
     labels: ["Guests", "Staff"],
-    datasets: [{
-      label: "Returning Vs New",
-      data: [100, 50],
-      backgroundColor: ["#64E4B1", "#008650"],
-      borderWidth: 0,
-    }],
+    datasets: [
+      {
+        label: "Returning Vs New",
+        data: [100, 50],
+        backgroundColor: ["#64E4B1", "#008650"],
+        borderWidth: 0,
+      },
+    ],
   },
   options: {
     cutout: 70,
     plugins: {
-      legend: { position: "bottom" },
-      title: { display: true, text: "Returning Vs New" },
+      legend: {
+        position: "bottom",
+        labels: {
+          color: "#ffffff",
+        },
+      },
+      title: { display: true, text: "Returning Vs New", color: "#ffffff" },
     },
   },
 });
@@ -84,22 +137,28 @@ let Office_Activity = new Chart(officeActivity, {
   type: "doughnut",
   data: {
     labels: ["Guests", "Staff"],
-    datasets: [{
-      label: "Office Activity",
-      data: [100, 50],
-      backgroundColor: ["#64E4B1", "#008650"],
-      borderWidth: 0,
-    }],
+    datasets: [
+      {
+        label: "Office Activity",
+        data: [100, 50],
+        backgroundColor: ["#64E4B1", "#008650"],
+        borderWidth: 0,
+      },
+    ],
   },
   options: {
     cutout: 70,
     plugins: {
-      legend: { position: "bottom" },
-      title: { display: true, text: "Office Activity" },
+      legend: {
+        position: "bottom",
+        labels: {
+          color: "#ffffff",
+        },
+      },
+      title: { display: true, text: "Office Activity", color: "#ffffff" },
     },
   },
 });
-
 
 window.onload = async () => {
   const interns = await getInternList();
@@ -121,16 +180,18 @@ async function getInternList() {
 
 async function exportCSV(data) {}
 
-function renderInterns(interns){
+function renderInterns(interns) {
   listContainer.innerHTML = "";
 
   Object.entries(interns).forEach(([name, info]) => {
-    const personDiv = document.createElement('div');
+    const personDiv = document.createElement("div");
     personDiv.className = "person";
 
     personDiv.innerHTML = `
       <div class="person-top">
-        <p>${info.honorifics || "Mr."} ${info["full name"]} ${info.suffix || ""}</p>
+        <p>${info.honorifics || "Mr."} ${info["full name"]} ${
+      info.suffix || ""
+    }</p>
         <p>${info.logs[0].date}</p>
         <p>Time-in: ${info.logs[0].timeIn}</p>
       </div>
@@ -148,7 +209,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const tab1Btn = document.querySelector(".tab1");
   const tab2Btn = document.querySelector(".tab2");
   const morphPath = document.getElementById("morphPath");
-  const backButton = document.getElementById('admin-back-button');
+  const backButton = document.getElementById("admin-back-button");
 
   const d1 = `M15 1.25H199C206.87 1.25 213.25 7.62994 213.25 15.5V40C213.25 48.6985 220.302 55.75 229 55.75H634C641.87 55.75 648.25 62.1299 648.25 70V517C648.25 524.87 641.87 531.25 634 531.25H15C7.12994 531.25 0.75 524.87 0.75 517V15.5C0.750003 7.62995 7.12995 1.25 15 1.25Z`;
 
@@ -172,7 +233,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const toggleActiveTab = (tab) => {
     document.querySelectorAll(".admin-tabs button").forEach((btn) => {
-      btn.classList.remove("active")
+      btn.classList.remove("active");
     });
     tab.classList.add("active");
 
@@ -187,7 +248,6 @@ document.addEventListener("DOMContentLoaded", () => {
       barContainer.classList.add("active");
     }
   };
-
 
   tab1Btn.addEventListener("click", () => {
     if (current !== "tab1") {
@@ -205,8 +265,8 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  backButton.addEventListener('click', () => {
-    window.location.href = '../sessionpage/sessionPage.html';
+  backButton.addEventListener("click", () => {
+    window.location.href = "../sessionpage/sessionPage.html";
   });
 
   searchBar.addEventListener("input", async () => {
@@ -218,7 +278,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const filtered = Object.fromEntries(
       Object.entries(interns).filter(([name]) =>
         name.toLowerCase().includes(searchTerm)
-      ));
+      )
+    );
 
     renderInterns(filtered);
   });
