20a16af6f40ac0521ad4f886471d2cc0c64cb5e3
HansongProgramming <hlf1850@students.uc-bcf.edu.ph>
2025-07-01 15:37:36 +0800

    changes to front-end: fixed tab bug

--- pages/adminlogin/index.js
+++ pages/adminlogin/index.js
@@ -200,18 +200,26 @@ searchBar.addEventListener("input", async () => {
 });
 
 
-const d1 = `M15 1.25H199C206.87 1.25 213.25 7.62994 213.25 15.5V40C213.25 48.6985 220.302 55.75 229 55.75H634C641.87 55.75 648.25 62.1299 648.25 70V517C648.25 524.87 641.87 531.25 634 531.25H15C7.12994 531.25 0.75 524.87 0.75 517V15.5C0.750003 7.62995 7.12995 1.25 15 1.25Z`;
-const d2 = `M225.5 0.75H340.5C348.37 0.75 354.75 7.12994 354.75 15V39.5C354.75 48.1985 361.802 55.25 370.5 55.25H634C641.87 55.25 648.25 61.6299 648.25 69.5V516.5C648.25 524.37 641.87 530.75 634 530.75H15C7.12994 530.75 0.75 524.37 0.75 516.5V69.5C0.75 61.6299 7.12994 55.25 15 55.25H195.5C204.198 55.25 211.25 48.1985 211.25 39.5V15C211.25 7.12994 217.63 0.75 225.5 0.75Z`;
+  const d1 = `M15 1.25H199C206.87 1.25 213.25 7.62994 213.25 15.5V40C213.25 48.6985 220.302 55.75 229 55.75H634C641.87 55.75 648.25 62.1299 648.25 70V517C648.25 524.87 641.87 531.25 634 531.25H15C7.12994 531.25 0.75 524.87 0.75 517V15.5C0.750003 7.62995 7.12995 1.25 15 1.25Z`;
 
-function animateMorph(from, to, duration = 800) {
+  const d2 = `M225.5 0.75H340.5C348.37 0.75 354.75 7.12994 354.75 15V39.5C354.75 48.1985 361.802 55.25 370.5 55.25H634C641.87 55.25 648.25 61.6299 648.25 69.5V516.5C648.25 524.37 641.87 530.75 634 530.75H15C7.12994 530.75 0.75 524.37 0.75 516.5V69.5C0.75 61.6299 7.12994 55.25 15 55.25H195.5C204.198 55.25 211.25 48.1985 211.25 39.5V15C211.25 7.12994 217.63 0.75 225.5 0.75Z`;
+
+function animateMorph(from, to, duration = 80) {
   const interpolator = flubber.interpolate(from, to);
-  let frame = 0;
-  const totalFrames = Math.round(duration / (6000 / 60));
+  let startTime = null;
 
-  const step = () => {
-    morphPath.setAttribute("d", interpolator(frame / totalFrames));
-    if (++frame < totalFrames) requestAnimationFrame(step);
+  const step = (timestamp) => {
+    if (!startTime) startTime = timestamp;
+    const elapsed = timestamp - startTime;
+    const t = Math.min(elapsed / duration, 1); // clamp to [0, 1]
+
+    morphPath.setAttribute("d", interpolator(t));
+
+    if (t < 1) {
+      requestAnimationFrame(step);
+    }
   };
+
   requestAnimationFrame(step);
 }
 
